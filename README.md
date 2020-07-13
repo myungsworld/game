c463c6ca408fc57024f5eff8e5090f4a44c4470f
myungsworld <myungsworld@gmail.com>
2020-07-13 10:51:22 +0900

    Memory game

--- README.md
+++ README.md
@@ -11,10 +11,4 @@ Learn:
 - For loops  
 - createElement() 
 
-nodejs는 DOM형식을 지원하지 않으므로 새로운 API를 받아야 한다 (ex. JSDOM)  
-document relates to the DOM (Document Object Model) in a web browser.  
-Node.js, however, is not browser Javascript. It is a server, much like PHP or Perl, and as such, you can't access the browser's DOM or do anything specific to browser-based Javascript.  
-The closest you could get is using something like browserify to include Node.js modules in your client-side code.  
-document is not defined in NodeJS, JavaScript doesn't have a default document global. Browsers provide one, but your Node.js code doesn't run in a browser, it runs in the Node.js  document is not defined. I'm using next.js. Please see my code below. Browse other questions tagged node.js reactjs next.js or ask your own question.  
-
 
--- memoryGame/app.js
+++ memoryGame/app.js
@@ -1,68 +1,161 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+// const DOMContentLoaded = document.addEventListener('DOMContentLoaded', ()=>{
 
-    //card options
-    const cardArray = [
-        {
-            name:'hotdog',
-            img : 'imges/hotdog.png'
-        },
-        {
-            name:'hotdog',
-            img : 'imges/hotdog.png'
-        },
-        {
-            name:'hotdog2',
-            img : 'imges/hotdog2.png'
-        },
-        {
-            name:'hotdog2',
-            img : 'imges/hotdog2.png'
-        },
-        {
-            name:'hotdog3',
-            img : 'imges/hotdog3.png'
-        },
-        {
-            name:'hotdog3',
-            img : 'imges/hotdog3.png'
-        },
-        {
-            name:'hotdog4',
-            img : 'imges/hotdog4.png'
-        },
-        {
-            name:'hotdog4',
-            img : 'imges/hotdog4.png'
-        },
-        {
-            name:'hotdog5',
-            img : 'imges/hotdog5.png'
-        },
-        {
-            name:'hotdog5',
-            img : 'imges/hotdog5.png'
-        },
-        {
-            name:'hotdog6',
-            img : 'imges/hotdog6.png'
-        },
-        {
-            name:'hotdog6',
-            img : 'imges/hotdog6.png'
-        }
-    ]
- })
+//     //card options
+//     const cardArray = [
+//         {
+//             name:'hotdog',
+//             img : 'imges/hotdog.png'
+//         },
+//         {
+//             name:'hotdog',
+//             img : 'imges/hotdog.png'
+//         },
+//         {
+//             name:'hotdog2',
+//             img : 'imges/hotdog2.png'
+//         },
+//         {
+//             name:'hotdog2',
+//             img : 'imges/hotdog2.png'
+//         },
+//         {
+//             name:'hotdog3',
+//             img : 'imges/hotdog3.png'
+//         },
+//         {
+//             name:'hotdog3',
+//             img : 'imges/hotdog3.png'
+//         },
+//         {
+//             name:'hotdog4',
+//             img : 'imges/hotdog4.png'
+//         },
+//         {
+//             name:'hotdog4',
+//             img : 'imges/hotdog4.png'
+//         },
+//         {
+//             name:'hotdog5',
+//             img : 'imges/hotdog5.png'
+//         },
+//         {
+//             name:'hotdog5',
+//             img : 'imges/hotdog5.png'
+//         },
+//         {
+//             name:'hotdog6',
+//             img : 'imges/hotdog6.png'
+//         },
+//         {
+//             name:'hotdog6',
+//             img : 'imges/hotdog6.png'
+//         }
+//     ]
+//  });
 
- const grid = document.querySelector('.grid')
 
- function createBoard() {
-    for (let i = 0; i < 12; i++){
+
+const cardArray = [
+    {
+        name:'hotdog',
+        img : 'images/hotdog.png'
+    },
+    {
+        name:'hotdog',
+        img : 'images/hotdog.png'
+    },
+    {
+        name:'hotdog2',
+        img : 'images/hotdog2.png'
+    },
+    {
+        name:'hotdog2',
+        img : 'images/hotdog2.png'
+    },
+    {
+        name:'hotdog3',
+        img : 'images/hotdog3.png'
+    },
+    {
+        name:'hotdog3',
+        img : 'images/hotdog3.png'
+    },
+    {
+        name:'hotdog4',
+        img : 'images/hotdog4.png'
+    },
+    {
+        name:'hotdog4',
+        img : 'images/hotdog4.png'
+    },
+    {
+        name:'hotdog5',
+        img : 'images/hotdog5.png'
+    },
+    {
+        name:'hotdog5',
+        img : 'images/hotdog5.png'
+    },
+    {
+        name:'hotdog6',
+        img : 'images/hotdog6.png'
+    },
+    {
+        name:'hotdog6',
+        img : 'images/hotdog6.png'
+    }
+]
+
+cardArray.sort(() => 0.5 - Math.random());
+
+const grid = document.querySelector('.grid')
+const resultDisplay = document.querySelector('#result')
+
+
+var cardsChosen = []
+var cardsChosenId = []
+var cardsWon = []
+
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++){
         var card = document.createElement('img')
         card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
-        //card.addEventListener('click',flipcard)
+        card.addEventListener('click',flipcard)
         grid.appendChild(card)
     }
+ }
+
+function checkForMAtch() {
+    var cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    if (cardsChosen[0] === cardsChosen[1]){
+        alert('You found a match')
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('Sorry, try again')
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if(cardsWon.length === cardArray.length/2){
+        resultDisplay.textContent ='Congratulations! You found them all!'
+    }
+}
+
+function flipcard(){
+     var cardId = this.getAttribute('data-id')
+     cardsChosen.push(cardArray[cardId].name)
+     cardsChosenId.push(cardId)
+     this.setAttribute('src', cardArray[cardId].img)
+     if (cardsChosen.length === 2) {
+         setTimeout(checkForMAtch, 500)
+     }
  }
 
 createBoard();
